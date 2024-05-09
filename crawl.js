7cf86dde7bf44a793cc930704f0e98e2644241d5
evswanson <evanswanson98@gmail.com>
2024-05-09 17:56:34 -0500

    Created function to crawl url and count all the links

--- crawl.js
+++ crawl.js
@@ -9,7 +9,7 @@ function normalizeURL(url) {
   return page;
 }
 
-function getURLsFromHTML(htmlBody, baseURL) {
+async function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
   const aElements = dom.window.document.querySelectorAll("a");
@@ -32,7 +32,61 @@ function getURLsFromHTML(htmlBody, baseURL) {
   return urls;
 }
 
+// Recursive function
+// currentURL will change while baseURL stays the same
+async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+  // 1. make sure currentURL is on the same domain as baseURL. IF not return pages
+  if (!currentURL.includes(baseURL)) {
+    return pages;
+  }
+  // 2. Get normalized version of currentURL
+  let normalized = await normalizeURL(currentURL);
+  // 3. If pages object already has an entry for the normalized version of the currentURL, increment the count and return pages
+  if (normalized in pages) {
+    pages[normalized].count++;
+    return;
+  } else {
+    // 4. Otherwise add an etry to the pages object and set the count to 1
+    pages[normalized] = { count: 1 };
+  }
+  // 5. Fetch the current URL and parse the html
+  let htmlBody = await getHtmlBody(currentURL);
+  // 6. Assuming all went well with the fetch, get all the URLs from the htmlBody
+  let htmlURLs = await getURLsFromHTML(htmlBody, currentURL);
+  // 7. Recursively crawl each URL found on the page and update the pages to keep an aggregate count.
+  for (const url of htmlURLs) {
+    pages = Object.assign({}, pages, await crawlPage(baseURL, url, pages));
+  }
+  // 8. Finally return the updated pages object
+  return pages;
+}
+
+// fetch and parse the html of the currentURL
+async function getHtmlBody(currentURL) {
+  //console.log(`crawling ${currentURL}`);
+  let response;
+  try {
+    response = await fetch(currentURL);
+  } catch (err) {
+    throw new Error(`Got Network error: ${err.message}`);
+  }
+
+  if (response.status > 399) {
+    console.log(`Got HTTP error: ${response.status} ${response.statusText}`);
+    return;
+  }
+
+  const contentType = response.headers.get("content-type");
+  if (!contentType || !contentType.includes("text/html")) {
+    console.log(`Got non-HTML response: ${contentType}`);
+    return;
+  }
+
+  //console.log(await response.text());
+  return response.text();
+}
 module.exports = {
   normalizeURL,
   getURLsFromHTML,
+  crawlPage,
 };
--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("hello world");
+const { crawlPage } = require("./crawl.js");
+
+async function main() {
+  // CLI arguments start at the 2nd element
+  let args = process.argv.slice(2);
+  // if number of CLI arguments is less than 1, print an error and exit.
+  if (args.length < 1) {
+    console.error("Process cancelled. Need to include a url after 'start'.");
+    console.log("---------------------------------------------");
+    return;
+  }
+  if (args.length > 1) {
+    // if number of CLI arguments is more than 1, print an error and exit.
+    console.error(
+      "Process cancelled. You can ONLY include ONE url after 'start'."
+    );
+    console.log("---------------------------------------------");
+    return;
+  }
+  // if we have exactly one CLI argument, it's the "baseURL"
+  let baseURL = args[0];
+  console.log("---------------------------------------------");
+  console.log("Process Started. Parsing URL:", baseURL);
+  console.log("---------------------------------------------");
+  // crawlPage(baseURL, currentURL=baseURL, pages={}) - Recursive function
+  let pages = await crawlPage(baseURL);
+  console.log("Pages:", pages);
+  console.log("----------------Parsing End---------------");
+}
+
+main();
