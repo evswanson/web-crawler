295be7f90a31e103f44e2c3fcee1604388c0b4ac
evswanson <evanswanson98@gmail.com>
2024-05-14 10:47:31 -0500

    added a report file to handle printing pages to the console

---
+++ report.js
@@ -0,0 +1,7 @@
+// convert pages object into something that can be logged into a console
+function printReport(pages) {
+  // 1. print that the report is starting
+  console.log("Report is starting");
+  // 2. sort the pages largest number -> DESC
+  // 3. print each page in a nice formatted way. Found ${count} internal links to ${url}
+}
